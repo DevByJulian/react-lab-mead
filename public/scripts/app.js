b7f9449f3adc73a8d170cc09a5ffd7c5eac3db42
Osajulian <you@example.com>
2018-08-21 08:19:29 -0400

    Arrow functions-2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,40 @@
 'use strict';
 
-// function square(x) {
-//   return x * x;
-// };
+// arguments object - no longer bound with arrow functions
 
-// console.log(square(3));
+var add = function add(a, b) {
+  // console.log(arguments);
+  return a + b;
+};
+console.log(add(55, 1, 1001));
 
-// // const squareArrow = (x) => {
-// //   return x * x;
-// // };
+// this keyword - no longer bound
 
-// const squareArrow = (x) => x * x;
+var user = {
+  name: 'Andrew',
+  cities: ['Philadelphia', 'New York', 'Dublin'],
+  printPlacesLived: function printPlacesLived() {
+    var _this = this;
 
-// console.log(squareArrow(4));
+    return this.cities.map(function (city) {
+      return _this.name + ' has lived in ' + city;
+    });
+  }
+};
+console.log(user.printPlacesLived());
 
+// Challenge area
 
-// Challenge - Use arrow functions
-// getFirstName('Mike Smith') -> "Mike"
-// Create regular arrow function
-// Create arrow function using shorthand syntax
+var multiplier = {
+  numbers: [10, 20, 30],
+  multiplyBy: 3,
+  multiply: function multiply() {
+    var _this2 = this;
 
-// const getFirstName = (fullName) => {
-//   return fullName.split(' ')[0];
-// };
-
-var getFirstName = function getFirstName(fullName) {
-  return fullName.split(' ')[0];
+    return this.numbers.map(function (number) {
+      return number * _this2.multiplyBy;
+    });
+  }
 };
 
-console.log(getFirstName('Andrew Mead'));
+console.log(multiplier.multiply());
